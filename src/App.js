ed853452c0a73c6e5629f542ad30c440e1265517
Gregory Gonzales-Landry <gglandry23@gmail.com>
2021-12-01 17:43:45 -0600

    fixed auth

--- src/App.js
+++ src/App.js
@@ -3,32 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-import Amplify from '@aws-amplify/core';
-Amplify.configure(awsExports);
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
-  if (!isPassedToWithAuthenticator) {
-    throw new Error('isPassedToWithAuthenticator was not provided');
-  }
-
-  return (
+function App({ withAuthenticator, signOut}) {
+  return (    
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
         <button onClick={signOut}>Sign Out</button>
       </header>
+      <signOut />
     </div>
+    
   );
 }
 
 export default withAuthenticator(App);
-
-export async function getStaticProps() {
-  return {
-    props: {
-      isPassedToWithAuthenticator: true,
-    },
-  };
-}
